6ead6694439ff90d80d54756fcc82f06c9856649
Alan <tyujn24@mail.ru>
2024-04-11 19:03:03 +0300

    add parser

--- dags/test/test_parcer.py
+++ dags/test/test_parcer.py
@@ -39,7 +39,7 @@ DEFAULT_ARGS = {
     "depends_on_past": False,
     "email_on_failure": False,
     "email_on_retry": False,
-    "retries": 2,
+    "retries": 1,
     "retry_delay": timedelta(minutes=1),
 }
 
